85c0cf9d5a69dfa34236db051947bc26cc61a3d3
acalva <abelcalva@gmail.com>
2013-08-18 19:57:44 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MslDaw::Application.config.secret_token = '00025717ef94f20c1bb24d2968eacc93a785d73d8aea14ddea381966014f8e350ebf5c945dda2803bc30cded0f2b97776c1dfba6046a1013d1a4e0ae1c22b053'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MslDaw::Application.config.session_store :cookie_store, key: '_msl-daw_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MslDaw::Application.config.session_store :active_record_store
